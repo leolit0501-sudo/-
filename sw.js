6e2178793944e4420469f35b0c8629b1fe3db75a
leolit0501-sudo <leolit0501@gmail.com>
2026-09-16 00:39:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const ASSETS = [
   './index.html',
   './app.js',
   './manifest.json',
-  'https://cdn.tailwindcss.com',
+  'https://cdn.tailwindcss.com'
 ];
 
 self.addEventListener('install', (e) => {
